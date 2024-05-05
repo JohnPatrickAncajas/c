4a6350aa71946d8a2b442b96f2925a47d37038db
JohnPatrickAncajas <143093214+JohnPatrickAncajas@users.noreply.github.com>
2024-05-05 23:47:50 +0800

    Update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link, Outlet } from 'react-router-dom';
-import { Navbar, Nav, Container, Modal, Button } from 'react-bootstrap';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 function MyNavbar() {
-  const [showModal, setShowModal] = useState(false);
-  const handleClose = () => setShowModal(false);
-  const handleShow = () => setShowModal(true);
-
   return (
     <Navbar expand="lg" bg="dark" variant="dark">
-      <Container>
+      <Container fluid>
         <Navbar.Brand as={Link} to="/">C Lesson Guide</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
@@ -18,27 +15,27 @@ function MyNavbar() {
           <Nav className="me-auto">
             <Nav.Link as={Link} to="/">Home</Nav.Link>
             <Nav.Link as={Link} to="/Lessons">Lessons</Nav.Link>
-            <Nav.Link onClick={handleShow}>Menu</Nav.Link>
+            <NavDropdown
+              id="nav-dropdown-dark-example"
+              title="Menu"
+              drop= "down-start"
+              menuVariant="dark"
+            >
+              <NavDropdown.ItemText style={{ minWidth: 'calc(100vw - 25.6px)' }}>Lesson 1 - Basic Computer Concepts</NavDropdown.ItemText>
+              <NavDropdown.Item href="#action/3.2" >Topic 1 - Computers</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Topic 2 - something long</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.4">Topic 3 - something long</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.5">Topic 4 - something long</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
             <Nav.Link as={Link} to="/Contact">Contact</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
       <Outlet />
-
-      <Modal show={showModal} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Menu</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <p><Link to="/Lessons/Lesson1" onClick={handleClose}>Lesson 1 - Basic Computer Concept</Link></p>
-          <p><Link to="/Lessons/Lesson2" onClick={handleClose}>Lesson 2 - More Advanced Concepts...</Link></p>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-        </Modal.Footer>
-      </Modal>
     </Navbar>
   );
 }
