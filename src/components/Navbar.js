aa57710e1841a021afed31ce8f79d71745b16e0c
JohnPatrickAncajas <143093214+JohnPatrickAncajas@users.noreply.github.com>
2024-05-07 22:25:16 +0800

    Update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,60 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, Outlet } from 'react-router-dom';
-import { Navbar, Nav, Container } from 'react-bootstrap';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Navbar, Nav, Container, Offcanvas} from 'react-bootstrap';
 
 function MyNavbar() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
-    <Navbar expand="lg" bg="dark" variant="dark">
-      <Container fluid>
-        <Navbar.Brand as={Link} to="/">C Lesson Guide</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ml-auto flex-grow-1">
-          </Nav>
-          <Nav className="me-auto">
-            <Nav.Link as={Link} to="/">Home</Nav.Link>
-            <Nav.Link as={Link} to="/Lessons">Lessons</Nav.Link>
-            <NavDropdown
-              id="nav-dropdown-dark-example"
-              title="Menu"
-              drop= "down-start"
-              menuVariant="dark"
-            >
-              <NavDropdown.ItemText style={{ minWidth: 'calc(100vw - 25.6px)' }}>Lesson 1 - Basic Computer Concepts</NavDropdown.ItemText>
-              <NavDropdown.Item href="#action/3.2" >Topic 1 - Computers</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Topic 2 - something long</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.4">Topic 3 - something long</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.5">Topic 4 - something long</NavDropdown.Item>
-              <NavDropdown.Divider />
+    <>
+      <Navbar expand="lg" bg="dark" variant="dark">
+        <Container fluid>
+          <Navbar.Brand as={Link} to="/">C Lesson Guide</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto flex-grow-1">
+            </Nav>
+            <Nav className="me-auto">
+              <Nav.Link as={Link} to="/">Home</Nav.Link>
+              <Nav.Link as={Link} to="/Lessons">Lessons</Nav.Link>
+              <Nav.Link onClick={handleShow}>Menu</Nav.Link>
+              <Nav.Link as={Link} to="/Contact">Contact</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+        <Outlet />
+      </Navbar>
 
-              <NavDropdown.ItemText>Lesson 1 - Basic Computer Concepts</NavDropdown.ItemText>
-              <NavDropdown.Item href="#action/3.2" >Topic 1 - Computers</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Topic 2 - something long</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.4">Topic 3 - something long</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.5">Topic 4 - something long</NavDropdown.Item>
-              <NavDropdown.Divider />
-
-              <NavDropdown.ItemText>Lesson 1 - Basic Computer Concepts</NavDropdown.ItemText>
-              <NavDropdown.Item href="#action/3.2" >Topic 1 - Computers</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Topic 2 - something long</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.4">Topic 3 - something long</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.5">Topic 4 - something long</NavDropdown.Item>
-              <NavDropdown.Divider />
-              
-              <NavDropdown.ItemText>Lesson 1 - Basic Computer Concepts</NavDropdown.ItemText>
-              <NavDropdown.Item href="#action/3.2" >Topic 1 - Computers</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Topic 2 - something long</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.4">Topic 3 - something long</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.5">Topic 4 - something long</NavDropdown.Item>
-              <NavDropdown.Divider />
-            </NavDropdown>
-            <Nav.Link as={Link} to="/Contact">Contact</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-      <Outlet />
-    </Navbar>
+      <Offcanvas show={show} onHide={handleClose} placement="end" className="bg-dark text-white">
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title>Menu</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+          <Nav.Link as={Link} to="/topic1">Topic 1 - Computers</Nav.Link>
+          <Nav.Link as={Link} to="/topic2">Topic 2 - something long</Nav.Link>
+        </Offcanvas.Body>
+      </Offcanvas>
+    </>
   );
 }
 
